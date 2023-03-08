5cbdaea91da8152d3d7c8539626774182856274a
brunogomescarvalho <7bruno8@gmail.com>
2023-03-08 00:54:28 -0300

    agora com dicas

--- Jogada.cs
+++ Jogada.cs
@@ -1,39 +1,34 @@
-namespace JogoDaForca
+public class Jogada
 {
-    public partial class Program
+    public string Palavra { get; set; }
+    public string Dica { get; set; }
+
+
+    public Jogada(string palavra, string dica)
     {
-        public class Jogada
-        {
-            public string Palavra { get; set; }
-            public string Dica { get; set; }
+        this.Palavra = palavra;
+        this.Dica = dica;
+    }
 
 
-            public Jogada(string palavra, string dica)
-            {
-                this.Palavra = palavra;
-                this.Dica = dica;
-            }
+    public static Jogada SortearJogada(List<Jogada> jogadas)
+    {
+        var random = new Random();
+        int index = random.Next(jogadas.Count);
+        return jogadas[index];
+    }
 
+    public static List<Jogada> ConstruirJogadas()
+    {
+        List<Jogada> jogadas = new List<Jogada>();
 
-            public static Jogada SortearJogada(List<Jogada> jogadas)
-            {
-                var random = new Random();
-                int index = random.Next(jogadas.Count);
-                return jogadas[index];
-            }
+        jogadas.Add(new Jogada("padaria", "Fabrica sabores e sonhos."));
+        jogadas.Add(new Jogada("edredom", "Companheiro inserparável no inverno."));
+        jogadas.Add(new Jogada("automovel", "Alguns tem cor de prata outros de ouro, mas todos tem bocharra."));
+        jogadas.Add(new Jogada("maionese", "É fácil de fazer mas vezes não da certo."));
 
-            public static List<Jogada> ConstruirJogadas()
-            {
-                List<Jogada> jogadas = new List<Jogada>();
-
-                jogadas.Add(new Jogada("padaria", "Fabrica sabores e sonhos."));
-                jogadas.Add(new Jogada("edredom", "Companheiro inserparável no inverno."));
-                jogadas.Add(new Jogada("automovel", "Alguns tem cor de prata outros de ouro, mas todos tem bocharra."));
-                jogadas.Add(new Jogada("maionese", "É fácil de fazer mas vezes não da certo."));
-
-                return jogadas;
-            }
-        }
+        return jogadas;
     }
 }
+
 
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace JogoDaForca
 {
-    public partial class Program
+    public class Program
     {
         static void Main(string[] args)
         {
